a213aa54c89748b877d16309d983725d35c6b80f
3atom1take1 <3atom1take1@gmail.com>
2022-03-10 14:13:40 +0900

    Slyghtly changed.

--- wrapper/wrapper.sh
+++
@@ -1,5 +0,0 @@
-python3 /home/ec2-user/property_valuation_calculator/script/scraping_suumo_baibai.py
-sleep 10s
-python3 /home/ec2-user/property_valuation_calculator/script/train_all_suumo_baibai.py
-sleep 10s
-python3 /home/ec2-user/property_valuation_calculator/script/predict_reasonable_price.py
